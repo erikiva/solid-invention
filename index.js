7bd59a24be90889d3c93d671514b510dd201bc14
natalia <nataliav@gmail.com>
2017-04-22 10:51:29 +0200

    Changed to promises and sync/await

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 const fs = require('fs');
+const fsp = require('fs-promise');
 
-function removeDir(path) {
+async function removeDir(path) {
     console.log('Removing dir ', path)
-    const list = fs.readdirSync(path);
+    const list = await fsp.readdir(path);
     for (let i = 0, length = list.length; i < length; i++){
         console.log(`Removing path ${path}/${list[i]}`);
-        const stats = fs.statSync(`${path}/${list[i]}`);
+        const stats = await fsp.stat(`${path}/${list[i]}`);
         if (stats.isDirectory()){
             console.log('Is a directory');
-            removeDir(`${path}/${list[i]}`);
+            await removeDir(`${path}/${list[i]}`);
         } else if (stats.isFile){
             console.log('Is a file');
-            fs.unlinkSync(`${path}/${list[i]}`);
+            await fsp.unlink(`${path}/${list[i]}`);
         }
     }
-    fs.rmdirSync(`${path}`);
+    await fsp.rmdir(`${path}`);
 }
 
 // node index.js myfolder Hola que tal
-function createScript(folderName, content){
+async function createScript(folderName, content){
     console.log(`Creating a folder with name ${folderName}`);
-    if (fs.existsSync(`${__dirname}/${folderName}`  )) {
+    const exists = await fsp.exists(`${__dirname}/${folderName}`);
+    if (exists) {
         console.log('Directory exists');
-        removeDir(`${__dirname}/${folderName}`)
+        await removeDir(`${__dirname}/${folderName}`)
     }
-    fs.mkdirSync(`${__dirname}/${folderName}`);
+    await fsp.mkdir(`${__dirname}/${folderName}`);
 
     console.log(`Writing file with content ${content}`);
-    fs.writeFileSync(`${__dirname}/${folderName}/file.txt`, content);
+    await fsp.writeFile(`${__dirname}/${folderName}/file.txt`, content);
 
     console.log('Reading file');
-    const buffer = fs.readFileSync(`${__dirname}/${folderName}/file.txt`, 'utf8');
+    const buffer = await fsp.readFile(`${__dirname}/${folderName}/file.txt`, 'utf8');
     console.log(`Read: ${buffer.toString('utf8')}`)
+    return 'OK from promise';
 }
 
-createScript(process.argv[2], process.argv.slice(3).join(' '));
+createScript(process.argv[2], process.argv.slice(3).join(' '))
+    .then((data)=>console.log('OK ', data), 
+        (err)=> {
+            console.error('Error', err); 
+            process.exit(1)}
+    );
